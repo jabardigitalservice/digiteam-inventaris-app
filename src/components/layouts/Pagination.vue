587bc933d78ad70a34fed3f07151f32ed506cf9f
Ray Fajar S <rayfajars.com>
2022-09-22 15:27:22 +0700

    add pagination

--- src/components/layouts/Pagination.vue
+++ src/components/layouts/Pagination.vue
@@ -1,5 +1,63 @@
-<script></script>
+<script>
+export default {
+  components: {},
+  props: ["pagination", "offset"],
+  computed: {
+    pagesNumber() {
+      // todo : i will fix it this code if API is done from backend
+      if (!this.pagination.to) {
+        return [];
+      }
+      let from = this.pagination.current_page - this.offset;
+      if (from < 1) {
+        from = 1;
+      }
+      let to = from + this.offset * 2;
+      if (to >= this.pagination.last_page) {
+        to = this.pagination.last_page;
+      }
+      let pagesArray = [];
+      for (let page = from; page <= to; page++) {
+        pagesArray.push(page);
+      }
+      return pagesArray;
+    },
+  },
+};
+</script>
 
 <template>
-  <div>asda</div>
+  <nav>
+    <ul class="inline-flex -space-x-px">
+      <li>
+        <a
+          href="#"
+          class="py-2 px-3 ml-0 leading-tight text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          >Previous</a
+        >
+      </li>
+
+      <li>
+        <a
+          v-for="page in pagesNumber"
+          :key="page"
+          href="#"
+          :class="
+            page == pagination.current_page
+              ? 'py-2 px-3 text-blue-600 bg-blue-50 border border-gray-300 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white'
+              : 'py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'
+          "
+          >{{ page }}</a
+        >
+      </li>
+
+      <li>
+        <a
+          href="#"
+          class="py-2 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          >Next</a
+        >
+      </li>
+    </ul>
+  </nav>
 </template>
