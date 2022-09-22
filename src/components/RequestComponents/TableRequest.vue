d8d00a4148f4b01fdb5a806f5b8825db6f21f16f
Ray Fajar S <rayfajars.com>
2022-09-22 15:27:47 +0700

    change color header table

--- src/components/RequestComponents/TableRequest.vue
+++ src/components/RequestComponents/TableRequest.vue
@@ -9,21 +9,21 @@ export default {
   <div class="w-full">
     <div class="border-b border-gray-200 shadow">
       <table class="divide-gray-300 w-full">
-        <thead class="bg-green-500">
+        <thead class="bg-green-200">
           <tr>
-            <th class="px-6 py-2 text-sm lg:text-md font-bold">No</th>
-            <th class="px-6 py-2 text-sm lg:text-md font-bold">
+            <th class="px-6 py-2 text-sm md:text-md font-bold">No</th>
+            <th class="px-6 py-2 text-sm md:text-md font-bold">
               Jenis Permohonan
             </th>
-            <th class="px-6 py-2 text-sm lg:text-md font-bold">Nama Pegawai</th>
-            <th class="px-6 py-2 text-sm lg:text-md font-bold">
+            <th class="px-6 py-2 text-sm md:text-md font-bold">Nama Pegawai</th>
+            <th class="px-6 py-2 text-sm md:text-md font-bold">
               Unit / Divisi
             </th>
-            <th class="px-6 py-2 text-sm lg:text-md font-bold">
+            <th class="px-6 py-2 text-sm md:text-md font-bold">
               Barang yang diajukan
             </th>
-            <th class="px-6 py-2 text-sm lg:text-md font-bold">Status</th>
-            <th class="px-6 py-2 text-sm lg:text-md font-bold">Action</th>
+            <th class="px-6 py-2 text-sm md:text-md font-bold">Status</th>
+            <th class="px-6 py-2 text-sm md:text-md font-bold">Action</th>
           </tr>
         </thead>
         <tbody class="bg-white divide-y divide-gray-300 text-center">
@@ -56,32 +56,47 @@ export default {
               <span
                 v-if="request.status == 1"
                 class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800"
-                >Pengajuan Masuk</span
-              >
+                >Pengajuan Masuk
+              </span>
+
               <span
                 v-else-if="request.status == 2"
                 class="bg-red-100 text-red-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-red-200 dark:text-red-900"
-                >Pengajuan Ditolak</span
+                >Pengajuan Ditolak
+              </span>
+
+              <span
+                v-else-if="request.status == 3"
+                class="bg-yellow-100 text-yellow-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-yellow-200 dark:text-yellow-900"
               >
+                Pengajuan Diterima
+              </span>
+
+              <span
+                v-else-if="request.status == 4"
+                class="bg-yellow-100 text-yellow-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-yellow-200 dark:text-yellow-900"
+              >
+                Permintaan Barang Masuk
+              </span>
+
+              <span
+                v-else-if="request.status == 5"
+                class="bg-yellow-100 text-yellow-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-yellow-200 dark:text-yellow-900"
+              >
+                Pengecekan Kelayakan
+              </span>
+
+              <span
+                v-else-if="request.status == 6"
+                class="bg-yellow-100 text-yellow-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-yellow-200 dark:text-yellow-900"
+              >
+                Barang Siap Diambil
+              </span>
 
               <span
                 v-else-if="request.status == 7"
                 class="bg-green-100 text-green-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-green-200 dark:text-green-900"
-                >Pengajuan Selesai</span
-              >
-
-              <span
-                v-else
-                class="bg-yellow-100 text-yellow-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-yellow-200 dark:text-yellow-900"
-              >
-                <span v-if="request.status == 3">Pengajuan Diterima</span>
-                <span v-else-if="request.status == 4"
-                  >Permintaan Barang Masuk</span
-                >
-                <span v-else-if="request.status == 5"
-                  >Pengecekan Kelayakan</span
-                >
-                <span v-else-if="request.status == 6">Barang Siap Diambil</span>
+                >Pengajuan Selesai
               </span>
             </td>
             <td class="px-6 py-4 text-sm">{{ request.id }}</td>
