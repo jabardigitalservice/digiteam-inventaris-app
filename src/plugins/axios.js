a4da29cb4da72a8507bbc5ce182a7e84120cdd04
Ray Fajar S <rayfajars.com>
2022-09-27 10:54:40 +0700

    remove red axios and try again code before add axios

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,4 +1,4 @@
-import axios from "redaxios";
+import axios from "axios";
 import Vue from "vue";
 
 const devInstance = createInstance(import.meta.env.VITE_API_URL);
@@ -14,6 +14,7 @@ function createInstance(baseURL) {
   });
 }
 
+console.log(devInstance);
 export default {
   install() {
     Vue.prototype.$axios = devInstance;
