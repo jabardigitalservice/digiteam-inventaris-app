71d79d54284a48496392d53d3bdc9b539ee3efef
Ray Fajar S <rayfajars.com>
2022-09-29 18:53:02 +0700

    add type and status component

---
+++ src/components/RequestComponents/TypeRequest.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  props: { requestType: Number },
+  methods: {
+    getTypeLabel(requestType) {
+      switch (requestType) {
+        case 1:
+          return "Baru";
+        case 2:
+          return "Tukar";
+        default:
+          return "-";
+      }
+    },
+    getColorLabel(requestType) {
+      switch (requestType) {
+        case 1:
+          return "bg-green-100 text-green-800 ";
+        case 2:
+          return "bg-yellow-100 text-yellow-800 ";
+        default:
+          return "-";
+      }
+    },
+  },
+};
+</script>
+
+<template>
+  <span
+    :class="getColorLabel(requestType)"
+    class="font-semibold mr-2 px-2.5 py-0.5 rounded"
+  >
+    {{ getTypeLabel(requestType) }}
+  </span>
+</template>
