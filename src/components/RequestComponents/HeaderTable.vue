5c36ff4097a264ab3e682bd3b14074887e8bb0a3
Ray Fajar S <rayfajars.com>
2022-09-29 18:53:53 +0700

    add componen header, table, titlecard

--- src/components/RequestComponents/HeaderTable.vue
+++ src/components/RequestComponents/HeaderTable.vue
@@ -3,10 +3,10 @@
 <template>
   <div class="mb-5">
     <router-link
+      class="text-blue-700 font-bold hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
       :to="{ name: 'add-request' }"
-      class="text-white font-bold bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
     >
-      Ajukan Permohonan
+      Tambah Permohonan
     </router-link>
   </div>
 </template>
--- src/views/layout/LoginLayout.vue
+++ src/views/layout/LoginLayout.vue
@@ -1,3 +1,9 @@
 <template>
   <router-view></router-view>
 </template>
+
+<style>
+body {
+  background: #e3f2fd;
+}
+</style>
