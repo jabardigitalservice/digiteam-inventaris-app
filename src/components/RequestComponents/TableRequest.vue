09409e4006e53578e93f7d797f83253d47348e69
Ray Fajar S <rayfajars.com>
2022-10-01 23:31:57 +0700

    add action column table

--- src/components/RequestComponents/TableRequest.vue
+++ src/components/RequestComponents/TableRequest.vue
@@ -4,6 +4,11 @@ import TypeRequest from "./TypeRequest.vue";
 export default {
   components: { StatusRequest, TypeRequest },
   props: { dataRequest: Array },
+  data() {
+    return {
+      show: false,
+    };
+  },
 };
 </script>
 
@@ -81,7 +86,66 @@ export default {
             <td class="px-6 py-4 border border-slate-200">
               <StatusRequest :status="request.status"></StatusRequest>
             </td>
-            <td class="px-6 py-4 border border-slate-200">{{ request.id }}</td>
+            <td class="px-6 py-4 border border-slate-200">
+              <div class="flex justify-center">
+                <div>
+                  <div class="dropdown">
+                    <button
+                      id="action Button"
+                      class="dropdown-toggle text-blue-700 font-bold hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 flex items-center whitespace-nowrap"
+                      type="button"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                      @click="show = !show"
+                    >
+                      Aksi
+                      <svg
+                        aria-hidden="true"
+                        focusable="false"
+                        data-prefix="fas"
+                        data-icon="caret-down"
+                        class="w-2 ml-2"
+                        role="img"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 320 512"
+                      >
+                        <path
+                          fill="currentColor"
+                          d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"
+                        ></path>
+                      </svg>
+                    </button>
+                    <ul
+                      v-show="show"
+                      class="dropdown-menu min-w-max absolute bg-white text-base z-50 float-left py-2 list-none text-left rounded-lg shadow-lg mt-1 m-0 bg-clip-padding border-none"
+                      aria-labelledby="action Button"
+                    >
+                      <li>
+                        <a
+                          class="dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100"
+                          href="#"
+                          >Detail</a
+                        >
+                      </li>
+                      <li>
+                        <a
+                          class="dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100"
+                          href="#"
+                          >Verifikasi</a
+                        >
+                      </li>
+                      <li>
+                        <a
+                          class="dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100"
+                          href="#"
+                          >Hapus</a
+                        >
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </td>
           </tr>
         </tbody>
       </table>
