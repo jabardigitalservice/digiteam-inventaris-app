60f44ef8ac77ae7d59b373402b8a365d64d336cb
Ray Fajar S <rayfajars.com>
2022-10-12 09:46:58 +0700

    add detail request and change api axios to gke

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 import Vue from "vue";
 import Cookies from "js-cookie";
-const productionInstance = createInstance(import.meta.env.VITE_STAGING_API_URL);
+const productionInstance = createInstance(
+  import.meta.env.VITE_STAGING_API_GKE_URL
+);
 function createInstance(baseURL) {
   return axios.create({
     baseURL,
