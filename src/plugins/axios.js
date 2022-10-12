6d86901a870b4bf8b51892d12a4a34ce92c86db6
Ray Fajar S <rayfajars.com>
2022-10-12 09:35:28 +0700

    remove useless vuexstore

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import Vue from "vue";
 import Cookies from "js-cookie";
-import VuexStore from "../store";
 const productionInstance = createInstance(import.meta.env.VITE_STAGING_API_URL);
 function createInstance(baseURL) {
   return axios.create({
