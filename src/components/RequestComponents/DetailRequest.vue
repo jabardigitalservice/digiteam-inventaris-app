57832a3140bef2bd8f90827b011fef8c5543c016
Ray Fajar S <rayfajars.com>
2022-10-12 10:43:15 +0700

    detail request dummy

--- src/components/RequestComponents/DetailRequest.vue
+++ src/components/RequestComponents/DetailRequest.vue
@@ -1,8 +1,9 @@
 <script>
 import Modal from "../layouts/Modal.vue";
 import TypeRequest from "./TypeRequest.vue";
+import StatusRequest from "./StatusRequest.vue";
 export default {
-  components: { Modal, TypeRequest },
+  components: { Modal, TypeRequest, StatusRequest },
   data() {
     return {
       titleModal: "Detail Data - Permohonan Inventaris",
@@ -20,7 +21,7 @@ export default {
       <div>
         <label
           for="request_type"
-          class="block mb-2 text-sm font-bold text-slate-700"
+          class="block mt-5 text-sm font-bold text-slate-700"
         >
           Jenis Permohonan
         </label>
@@ -31,6 +32,42 @@ export default {
           >
           <span>Ray</span>
         </label>
+        <label class="block mt-5">
+          <span class="block text-sm font-bold text-slate-700">No Telepon</span>
+          <span>08780000000</span>
+        </label>
+        <label class="block mt-5">
+          <span class="block text-sm font-bold text-slate-700"
+            >Unit / Divisi</span
+          >
+          <span>IT Development</span>
+        </label>
+        <label class="block mt-5">
+          <span class="block text-sm font-bold text-slate-700"
+            >Barang yang diajukan</span
+          >
+          <span>Laptop</span>
+        </label>
+        <label class="block mt-5">
+          <span class="block text-sm font-bold text-slate-700"
+            >Alasan Pengajuan</span
+          >
+          <span>Laptop saya rusak</span>
+        </label>
+        <label class="block mt-5">
+          <span class="block text-sm font-bold text-slate-700"
+            >Tingkat Kebutuhan</span
+          >
+          <span>Urgent</span>
+        </label>
+
+        <label
+          for="request_type"
+          class="block mt-5 text-sm font-bold text-slate-700"
+        >
+          Status Request
+        </label>
+        <StatusRequest :status="1" />
       </div>
     </template>
   </Modal>
