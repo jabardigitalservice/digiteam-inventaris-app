5001302b199ed261008726286c03ce1bfc8d3c04
Ray Fajar S <rayfajars.com>
2022-10-13 09:12:48 +0700

    change fetch data from plugins to utils api

--- src/main.js
+++ src/main.js
@@ -1,19 +1,11 @@
 import Vue from "vue";
-
 import App from "./App.vue";
 import router from "./router";
-
 import store from "./store";
-
 import "./assets/tailwind.css";
-
-import http from "./plugins/axios.js";
-
 import Swal from "./plugins/swal.js";
-
 import keycloak from "./plugins/keycloak.js";
 
-Vue.use(http);
 Vue.use(Swal);
 Vue.use(keycloak);
 
---
+++ src/utils/cookies.js
@@ -0,0 +1,9 @@
+import Cookies from "js-cookie";
+
+export function getToken() {
+  return Cookies.get("token");
+}
+
+export function removeToken() {
+  return Cookies.remove("token");
+}
