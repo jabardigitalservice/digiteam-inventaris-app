0f8f1cc1ba86b6dc1a5d475e4dba16f04152b555
Ray Fajar S <rayfajars.com>
2022-10-18 18:42:59 +0700

    add static data status request

---
+++ src/constants/index.js
@@ -0,0 +1,34 @@
+export const statusObject = Object.freeze({
+  PENGAJUAN_MASUK: {
+    value: 1,
+    text: "Pengajuan Masuk",
+  },
+  PENGAJUAN_DITOLAK: {
+    value: 2,
+    text: "Pengajuan Ditolak",
+  },
+  PENGAJUAN_DITERIMA: {
+    value: 3,
+    text: "Pengajuan Diterima",
+  },
+  PERMINTAAN_BARANG_MASUK: {
+    value: 4,
+    text: "Permintaan Barang Masuk",
+  },
+  PENGECEKAN_KELAYAKAN: {
+    value: 5,
+    text: "Pengecekan Kelayakan",
+  },
+  BARANG_SIAP_DIAMBIL: {
+    value: 6,
+    text: "Barang Siap Diambil",
+  },
+  PENGAJUAN_SELESAI: {
+    value: 7,
+    text: "Pengajuan Selesai",
+  },
+  PENGEMBALIAN_BARANG: {
+    value: 8,
+    text: "Barang Sudah dikembalikan",
+  },
+});
