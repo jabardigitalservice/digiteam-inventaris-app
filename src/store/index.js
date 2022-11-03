2396ee5c6420bf7a4b5167f1185626d2ac7c88c0
Ray Fajar S <rayfajars.com>
2022-11-03 09:38:19 +0700

    remove vuex send file change to helpres js

--- src/store/index.js
+++ src/store/index.js
@@ -10,22 +10,15 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     token: null,
-    fileName: "",
   },
   getters: {
     token(state) {
       return state.token;
     },
-    fileName(state) {
-      return state.fileName;
-    },
   },
   mutations: {
     SET_TOKEN(state, token) {
       state.token = token;
-    },
-    SET_FILENAME(state, fileName) {
-      state.fileName = fileName;
     },
   },
   actions: {
@@ -39,24 +32,6 @@ export default new Vuex.Store({
       Vue.$keycloak.logout({
         redirectUri: import.meta.env.VITE_STAGING_URI_BACK,
       });
-    },
-    async sendFile({ commit, dispatch }, file) {
-      try {
-        const response = await postFile("/files/upload", "POST", file);
-        commit("SET_FILENAME", response.data.filename);
-      } catch (error) {
-        dispatch(
-          "sweetalert/errorAlert",
-          {
-            title: "Server Error!",
-            text: "Gagal upload file!",
-          },
-          { root: true }
-        );
-      }
-    },
-    clearFile({ commit }) {
-      commit("SET_FILENAME", "");
     },
   },
   modules: { modals, sweetalert, user },
