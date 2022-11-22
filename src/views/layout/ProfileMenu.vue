8ccbf47f469f15375e9677bbd0f65b0631572227
Ray Fajar S <rayfajars.com>
2022-11-22 11:54:19 +0700

    add icon and edit css profile navbar

--- src/views/layout/ProfileMenu.vue
+++ src/views/layout/ProfileMenu.vue
@@ -19,16 +19,16 @@ export default {
 <template>
   <div class="relative">
     <button
-      class="bg-white focus:outline-none font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center"
-      :class="show ? 'text-blue-700' : 'text-[#788896]'"
+      class="bg-white focus:outline-none font-medium rounded-lg text-sm text-center inline-flex items-center"
+      :class="show ? 'text-blue-700 font-bold' : 'text-gray-800'"
       @click="show = !show"
     >
-      <span class="mr-4 font-bold"> {{ $store.state.user.profile.name }}</span>
+      <span class="mr-3"> {{ $store.state.user.profile.name }}</span>
 
       <IconSvg
         icon="/icons/arrow.svg"
         class="!w-5 !h-5"
-        :class="show ? '!bg-[#1976D2] rotate-90' : '!text-[#788896]'"
+        :class="show ? '!bg-blue-700 rotate-90' : '!bg-gray-800'"
       />
     </button>
 
@@ -37,10 +37,18 @@ export default {
       class="absolute right-0 py-2 bg-white rounded-md shadow-xl w-40"
     >
       <button
-        class="w-full px-4 py-2 text-sm text-[#788896] hover:bg-blue-100"
+        class="w-full inline-flex items-center text-sm hover:bg-blue-50 group"
         @click="logout"
       >
-        Keluar
+        <IconSvg
+          class="!bg-gray-800 group-hover:!bg-blue-700 ml-3 mr-3"
+          icon="/icons/Logout.svg"
+        />
+
+        <span
+          class="text-gray-800 group-hover:text-blue-700 group-hover:font-bold"
+          >Keluar</span
+        >
       </button>
     </div>
   </div>
