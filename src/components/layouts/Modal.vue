3927d453df1e549ebe2abec814ee0f13a60864e0
Ray Fajar S <rayfajars.com>
2022-11-22 13:00:43 +0700

    modal scrolablle

--- src/components/layouts/Modal.vue
+++ src/components/layouts/Modal.vue
@@ -22,52 +22,49 @@ export default {
 </script>
 
 <template>
-  <div>
-    <div
-      v-if="isOpen"
-      class="fixed inset-0 z-50 w-full max-h-full outline-none overflow-x-hidden overflow-y-auto focus:outline-none"
-    >
-      <div class="relative w-auto my-6 mx-auto max-w-2xl h-auto">
-        <div
-          class="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none"
-        >
-          <div
-            class="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t"
+  <div
+    v-if="isOpen"
+    class="fixed inset-0 z-50 w-full max-h-full bg-gray-500 bg-opacity-75"
+  >
+    <div class="relative mx-auto w-full h-full p-8 max-h-full max-w-2xl">
+      <div
+        class="flex flex-col overflow-hidden bg-white rounded w-full max-h-full"
+      >
+        <div class="flex items-center justify-between p-4 border-b">
+          <h5 class="text-xl font-semibold text-blue-gray-800 uppercase">
+            <slot name="header"> </slot>
+          </h5>
+          <button
+            class="p-1 ml-auto border-0 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+            @click="close"
           >
-            <h3 class="text-2xl font-semibold text-blue-gray-800 uppercase">
-              <slot name="header"> </slot>
-            </h3>
-            <button
-              class="p-1 ml-auto border-0 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-              @click="close"
+            <span
+              class="text-black h-6 w-6 text-xl block outline-none focus:outline-none"
             >
-              <span
-                class="text-black h-6 w-6 text-xl block outline-none focus:outline-none"
-              >
-                x
-              </span>
-            </button>
-          </div>
+              x
+            </span>
+          </button>
+        </div>
 
-          <div class="relative p-6 flex-auto">
+        <div class="relative overflow-y-auto p-4">
+          <div class="space-y-4">
             <slot name="body" />
           </div>
+        </div>
 
-          <div
-            class="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b"
+        <div
+          class="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b"
+        >
+          <button
+            class="text-red-800 border border-solid border-red-800 hover:bg-red-800 hover:text-white active:bg-red-900 font-bold uppercase text-sm px-6 py-3 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+            type="button"
+            @click="close"
           >
-            <button
-              class="text-red-800 border border-solid border-red-800 hover:bg-red-800 hover:text-white active:bg-red-900 font-bold uppercase text-sm px-6 py-3 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-              type="button"
-              @click="close"
-            >
-              Batal
-            </button>
-            <slot name="footer"></slot>
-          </div>
+            Batal
+          </button>
+          <slot name="footer"></slot>
         </div>
       </div>
     </div>
-    <div v-if="isOpen" class="opacity-25 fixed inset-0 z-40 bg-black"></div>
   </div>
 </template>
